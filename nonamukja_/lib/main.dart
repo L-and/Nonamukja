db05e073cb916b275282342234ca43f8bbbd0f53
L-and <ksw1812@naver.com>
2022-11-24 19:21:33 +0900

    feat: Add Routes
    스크린들의 루트 설정

--- nonamukja_/lib/main.dart
+++ nonamukja_/lib/main.dart
@@ -1,7 +1,23 @@
 import 'package:flutter/material.dart';
-import 'Views/login_view.dart';
-import 'Views/register_view.dart';
+import 'screens/screen_login.dart';
+import 'screens/screen_register.dart';
+import 'screens/screen_splash.dart';
 
 void main() {
-  runApp(const LoginView());
+  runApp(MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: '노나묵자',
+      routes: {
+        '/': (context) => SplashScreen(), // 로그인검사 화면
+        '/login': (context) => LoginScreen(), // 로그인 화면
+        '/register': (context) => RegisterScreen(), // 회원가입 화면
+      },
+      initialRoute: '/',
+    );
+  }
 }
