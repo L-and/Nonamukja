79ea6091d81008642aac148f0f99d7bcf7b789d9
L-and <ksw1812@naver.com>
2022-12-09 02:01:08 +0900

    faet: auth model

--- nonamukja_/lib/models/auth.dart
+++ nonamukja_/lib/models/auth.dart
@@ -1,42 +1,12 @@
 import 'package:flutter/material.dart';
 import 'package:http/http.dart' as http;
 
-class UserModel {
-  String _email = "";
-  String _password = "";
-  String _nickname = "";
+class Auth {
+  Map<String, dynamic> _json = {};
 
-  String get email => _email;
-  String get password => _password;
-  String get nickname => _nickname;
+  Map<String, dynamic> get json => _json;
 
-  set email(String input_email) {
-    _email = input_email;
-  }
-
-  set password(String input_password) {
-    _password = input_password;
-  }
-
-  set nickname(String input_nickname) {
-    _nickname = input_nickname;
-  }
-
-  void registerPostRequest() async { // 회원가입 POST 요청
-    Uri url = Uri.parse('http://think2022.iptime.org:9900/accounts/signup'); // 회원가입 POST 요청 주소
-
-    Map<String, String> registerJson = {
-      'email': _email,
-      'password': _password,
-      'nickname': _nickname
-    };
-
-    print(registerJson);
-
-    http.Response response = await http.post(
-      url,
-      body: registerJson,
-    );
-
+  set json(Map<String, dynamic> input_json) {
+    _json = input_json;
   }
 }
