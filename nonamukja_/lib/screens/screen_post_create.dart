5b90582d42734edad19a57755f3c9c7b51af147b
L-and <ksw1812@naver.com>
2022-12-19 03:03:50 +0900

    fix: UI fix

--- nonamukja_/lib/screens/screen_post_create.dart
+++ nonamukja_/lib/screens/screen_post_create.dart
@@ -1,51 +1,27 @@
 import 'package:flutter/material.dart';
-import 'package:nonamukja/singleton_class/post.dart';
-
-
+import 'package:image_picker/image_picker.dart';
 
 class PostCreateScreen extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      body: PostScrollView(),
+      appBar: PostCreateAppBar(),
+      body: PostCreateBody(),
     );
   }
-}
 
-Widget PostScrollView() {
-  Post post = Post(); // 포스트 불러오기
+  PostCreateAppBar() {
+    return AppBar(
+    );
+  }
 
-  return CustomScrollView(
-    slivers: <Widget>[
-      SliverAppBar( // 게시글 이미지
-        pinned: true,
-        expandedHeight: 250.0,
-        flexibleSpace: Stack(
-          children: <Widget>[
-          ],
-        ),
+  PostCreateBody() {
+    return Container(
+      child: Column(
+        children: [
+          Icon(Icons.photo),
+        ],
       ),
-      SliverToBoxAdapter( // 게시글 본문
-          child: Column(
-            crossAxisAlignment: CrossAxisAlignment.start,
-            children: [
-            ],
-          )
-      ),
-    ],
-  );
-}
-
-Container BottomBar() {
-  return Container(
-    color: Colors.grey,
-    height: 50,
-    padding: EdgeInsets.fromLTRB(5, 0, 5, 0),
-    child: Row(
-      children: [
-        Container(),
-        Container(),
-      ],
-    ),
-  );
+    );
+  }
 }
