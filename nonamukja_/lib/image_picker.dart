8f8fd27e8875470f51b3a7768a9ef10c25b12020
L-and <ksw1812@naver.com>
2022-12-21 20:14:10 +0900

    feat: image picker function

--- nonamukja_/lib/image_picker.dart
+++ nonamukja_/lib/image_picker.dart
@@ -2,11 +2,31 @@ import 'dart:io';
 import 'dart:ui';
 import 'package:image_picker/image_picker.dart';
 
-class ImageUploader {
+class MyImagePicker {
   final ImagePicker _picker = ImagePicker();
-  XFile? image;
+  XFile? _image;
 
-  SetImage() async {
-    image = await _picker.pickImage(source: ImageSource.gallery);
+  SetImageFromCam() async { // 카메라로 이미지 설정
+    var image = await _picker.pickImage(source: ImageSource.camera);
+
+    if(image != null) {
+      _image = image;
+    }
+  }
+
+  SetImageFromGallery() async {
+    var image = await _picker.pickImage(source: ImageSource.gallery);
+
+    if(image != null) {
+      _image = image;
+    }
+  }
+
+  GetImage() {
+    if(_image != null) {
+      return _image;
+    } else {
+      return null;
+    }
   }
 }
